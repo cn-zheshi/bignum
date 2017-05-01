1bc3c3badb24c7cd63f74d682a6251353b80c27e
潘昱光 <1760864645@qq.com>
2017-05-01 20:38:47 +0800

    增加乘法功能

--- bignum.hpp
+++ bignum.hpp
@@ -29,19 +29,43 @@ int p(char a)
 class bignum {
 	friend bignum operator+(const bignum&,const bignum&);
 	friend bignum operator-(const bignum& a);
+	friend bignum operator*(const bignum& a,const bignum& b);
 	std::deque<int> de1;
 	std::deque<int> de2;
 	bool sign=true;
-	char point='.';
 public:
-	bignum()
-	{
-		point=' ';
-	}
+	bignum()=default;
 	//template<typename T>bignum(T&& val)
 	bignum(long double val)
 	{
 		std::string str=std::to_string(val);
+		int count=0;
+		if(!str.empty()) {
+			if(!std::isalnum(str[0])) {
+				if(str[0]=='-')
+					sign=false;
+				for(int i=1; i<str.size(); ++i) {
+					if(!std::ispunct(str[i])&&count==0) {
+						de1.push_front(p(str[i]));
+					} else
+						++count;
+					if(count>1)
+						de2.push_back(p(str[i]));
+				}
+			} else {
+				for(int i=0; i<str.size(); ++i) {
+					if(!std::ispunct(str[i])&&count==0) {
+						de1.push_front(p(str[i]));
+					} else
+						++count;
+					if(count>1)
+						de2.push_back(p(str[i]));
+				}
+			}
+		}
+	}
+	bignum(const std::string& str)
+	{
 		int count=0;
 		if(!str.empty()) {
 			if(!std::isalnum(str[0])) {
@@ -149,14 +173,43 @@ bignum operator+(const bignum& a,const bignum& b)
 				if(i>s)
 					(c.de1).push_back(-(b.de1)[i]);
 			}
-			for(int i=0; i<(b.de2).size(); ++i)
-				(c.de2).push_back((a.de2)[i]-(b.de2)[i]);
+			int count=0;
+			while(count==0&&(c.de1).size()>1){
+				count=1;
+				if((c.de1)[(c.de1).size()-1]==0){
+					c.de1.pop_back();
+					--count;
+				}
+			}
+			s=(a.de2).size()>(b.de2).size()?(b.de2).size()-1:(a.de2).size()-1;
+			l=(a.de2).size()<(b.de2).size()?(b.de2).size()-1:(a.de2).size()-1;
+			for(int i=0; i<l; ++i){
+				if(i<=s)
+					(c.de2).push_back((a.de2)[i]-(b.de2)[i]);
+				if(l==a.de2.size()-1)
+					if(i>s)
+						c.de2.push_back((a.de2)[i]);
+				if(l==b.de2.size()-1)
+					if(i>s)
+						c.de2.push_back(-(b.de2)[i]);
+			}
 			if((c.de1)[(c.de1).size()-1]<0){
 				c.sign=!(c.sign);
 				for(int i=0; i<(b.de2).size(); ++i)
 					(c.de2)[i]=-(c.de2)[i];
 				for(int i=0; i<(c.de1).size(); ++i)
 					(c.de1)[i]=-(c.de1)[i];
+			}
+			if(c.de1.size()==1&&(c.de1)[0]==0){
+				for(int v=0;v<c.de2.size();++v)
+					if((c.de2)[v]<0){
+						c.sign=!(c.sign);
+						for(int i=0; i<(b.de2).size(); ++i)
+							(c.de2)[i]=-(c.de2)[i];
+						for(int i=0; i<(c.de1).size(); ++i)
+							(c.de1)[i]=-(c.de1)[i];
+						break;
+					}
 			}
 			for(int i=(c.de2).size()-1; i>0; --i) {
 				if(i!=0&&(c.de2)[i]<0) {
@@ -172,14 +225,6 @@ bignum operator+(const bignum& a,const bignum& b)
 				if((c.de1)[i]<0&&i!=(c.de1).size()-1) {
 					(c.de1)[i]+=10;
 					--(c.de1)[i+1];
-				}
-			}
-			int count=0;
-			while(count==0&&(c.de1).size()>1){
-				count=1;
-				if((c.de1)[(c.de1).size()-1]==0){
-					c.de1.pop_back();
-					--count;
 				}
 			}
 			return c;
@@ -197,14 +242,43 @@ bignum operator+(const bignum& a,const bignum& b)
 				if(i>s)
 					(c.de1).push_back((b.de1)[i]);
 			}
-			for(int i=0; i<(b.de2).size(); ++i)
-				(c.de2).push_back((b.de2)[i]-(a.de2)[i]);
+			int count=0;
+			while(count==0&&(c.de1).size()>1){
+				count=1;
+				if((c.de1)[(c.de1).size()-1]==0){
+					c.de1.pop_back();
+					--count;
+				}
+			}
+			s=(a.de2).size()>(b.de2).size()?(b.de2).size()-1:(a.de2).size()-1;
+			l=(a.de2).size()<(b.de2).size()?(b.de2).size()-1:(a.de2).size()-1;
+			for(int i=0; i<l; ++i){
+				if(i<=s)
+					(c.de2).push_back((b.de2)[i]-(a.de2)[i]);
+				if(l==a.de2.size()-1)
+					if(i>s)
+						c.de2.push_back(-(a.de2)[i]);
+				if(l==b.de2.size()-1)
+					if(i>s)
+						c.de2.push_back((b.de2)[i]);
+			}
 			if((c.de1)[(c.de1).size()-1]<0){
 				c.sign=!(c.sign);
 				for(int i=0; i<(b.de2).size(); ++i)
 					(c.de2)[i]=-(c.de2)[i];
 				for(int i=0; i<(c.de1).size(); ++i)
 					(c.de1)[i]=-(c.de1)[i];
+			}
+			if(c.de1.size()==1&&(c.de1)[0]==0){
+				for(int v=0;v<c.de2.size();++v)
+					if((c.de2)[v]<0){
+						c.sign=!(c.sign);
+						for(int i=0; i<(b.de2).size(); ++i)
+							(c.de2)[i]=-(c.de2)[i];
+						for(int i=0; i<(c.de1).size(); ++i)
+							(c.de1)[i]=-(c.de1)[i];
+						break;
+					}
 			}
 			for(int i=(c.de2).size()-1; i>0; --i) {
 				if(i!=0&&(c.de2)[i]<0) {
@@ -222,14 +296,6 @@ bignum operator+(const bignum& a,const bignum& b)
 					--(c.de1)[i+1];
 				}
 			}
-			int count=0;
-			while(count==0&&(c.de1).size()>1){
-				count=1;
-				if((c.de1)[(c.de1).size()-1]==0){
-					c.de1.pop_back();
-					--count;
-				}
-			}
 			return c;
 		}
 	}
@@ -242,4 +308,80 @@ bignum operator-(const bignum& a){
 bignum operator-(const bignum& a,const bignum& b){
 	bignum c=-b;
 	return a+c;
+}
+bignum operator*(const bignum& a,const bignum& b){
+	bignum c;
+	std::deque<int> x(a.de1.size()+b.de1.size(),0);
+	c.de1=x;
+	std::deque<int> y(a.de2.size()+b.de2.size(),0);
+	c.de2=y;
+	if(!((a.sign&&b.sign)||(!a.sign&&!b.sign)))
+		c.sign=false;
+	for(int i=0;i<a.de1.size();++i){
+		for(int v=0;v<b.de1.size();++v){
+			(c.de1)[i+v]+=((a.de1)[i])*((b.de1)[v]);
+		}
+	}
+	for(int i=0;i<a.de2.size();++i){
+		for(int v=0;v<b.de2.size();++v){
+			(c.de2)[i+v+1]+=((a.de2)[i])*((b.de2)[v]);
+		}
+	}
+	for(int i=0;i<a.de1.size();++i){
+		for(int v=0;v<b.de2.size();++v){
+			if(i>v)
+				(c.de1)[i-v-1]+=((a.de1)[i])*((b.de2)[v]);
+			else
+				(c.de2)[v-i]+=((a.de1)[i])*((b.de2)[v]);
+		}
+	}
+	for(int i=0;i<a.de2.size();++i){
+		for(int v=0;v<b.de1.size();++v){
+			if(i>=v)
+				(c.de2)[i-v]+=((a.de2)[i])*((b.de1)[v]);
+			else
+				(c.de1)[v-i-1]+=((a.de2)[i])*((b.de1)[v]);
+		}
+	}
+	int count=0;
+	while(count==0&&(c.de1).size()>1){
+		count=1;
+		if((c.de1)[(c.de1).size()-1]==0){
+			c.de1.pop_back();
+			--count;
+		}
+	}
+	count=1;
+	while(count!=0){
+		count=0;
+		for(int i=(c.de2).size()-1; i>0; --i) {
+			if(i!=0&&(c.de2)[i]>=10) {
+				(c.de2)[i]-=10;
+				++(c.de2)[i-1];
+				++count;
+			}
+			if((c.de2)[0]>=10) {
+				(c.de2)[0]-=10;
+				++(c.de1)[0];
+				++count;
+			}
+		}
+		for(int i=0; i<(c.de1).size(); ++i) {
+			if((c.de1)[i]>=10&&i!=(c.de1).size()-1) {
+				(c.de1)[i]-=10;
+				++(c.de1)[i+1];
+				++count;
+			}
+			if((c.de1)[i]>=10&&i==(c.de1).size()-1) {
+				(c.de1)[i]-=10;
+				(c.de1).push_back(1);
+				++count;
+			}
+		}
+	}
+	return c;
+}
+bignum operator""x(const char* p)
+{
+	return bignum(p);
 }
--- test.cpp
+++ test.cpp
@@ -1,8 +1,9 @@
 #include "bignum.hpp"
 int main(){
-	bignum a=467328.6473;
-	bignum b=-647328;
+	bignum a=28.6473;
+	bignum b=-24.21;
 	(a-b).print();
 	(a+b).print();
+	(a*b).print();
 	return 0;
 }
