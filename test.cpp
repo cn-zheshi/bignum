e3aedaa2ec777c76d1110c7aa5110439ee6ccc23
Michael Lee <mikecovlee@163.com>
2017-05-30 14:44:24 +0800

    改进代码可读性，改进输出方式

--- test.cpp
+++ test.cpp
@@ -1,11 +1,13 @@
 #include "bignum.hpp"
+#include <iostream>
 int main()
 {
-	bignum a=28;
-	bignum b=-24;
-	(a-b).print();
-	(a+b).print();
-	(a*b).print();
-	(a/b).print();
+	std::cout<<1234.56_bignum<<std::endl;
+	zheshi::bignum a=28;
+	zheshi::bignum b=-24;
+	std::cout<<a-b<<std::endl;
+	std::cout<<a+b<<std::endl;
+	std::cout<<a*b<<std::endl;
+	std::cout<<a/b<<std::endl;
 	return 0;
 }
